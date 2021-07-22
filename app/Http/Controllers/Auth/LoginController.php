2e0a31941ddfa89a2d65e8d5d6ec21b7183a872c
JP Timcang <josephpatricktimcang@gmail.com>
2021-07-22 15:57:13 +0800

    fixing the login of the system

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,11 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function username()
+    {
+        return 'user_name';
+    }
+
     /**
      * Where to redirect users after login.
      *
